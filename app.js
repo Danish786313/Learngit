3b84db5439541d77eefec60296f6bd32bb31638b
Danish ahmad Khan <mohddanishkhan681@gmail.com>
2022-09-12 18:29:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config()
+// const { sequelize } = require('./models');
+global.__basedir = __dirname + "/";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors())
+
+app.get('/master', (req, res) => {
+    res.status(200).json({
+        message: "Hello User!"
+    })
+})
+
+const port = (process.env.PORT || 4000)
+
+//Starting a server
+app.listen(port, async () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
